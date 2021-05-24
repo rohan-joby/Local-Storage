8a77789b978d99436967b1e75f4a1727db38d74e
Rohan Joby <rohanjobyak@gmail.com>
2021-05-24 23:48:28 +0530

    feat: add delete functionality

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function populateItems(items=[], itemList){
         <li>
         <input type="checkbox" data-index=${index} id="item${index}" ${item.done ? "checked":"" }/>
         <label for="item${index}">${item.text}</label>
+        <button type="button" name="delete" data-index=${index} style="border:none">⛔</button>
         </li>`
     }).join('');
 }
@@ -35,7 +36,18 @@ function eventToggle(e){
     populateItems(items,itemsList);
 }
 
+function handleDelete(e){
+    if(!e.target.matches('button'))return;
+    const element = e.target;
+    const index = element.dataset.index;
+    const deleteIndex = Number(index);
+    items.splice(deleteIndex,deleteIndex+1);
+    localStorage.setItem('items',JSON.stringify(items));
+    populateItems(items,itemsList);
+}
+
 addItems.addEventListener('submit',addItem);
 itemsList.addEventListener('click',eventToggle);
+itemsList.addEventListener('click',handleDelete);
 
 populateItems(items,itemsList);
