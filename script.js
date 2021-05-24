29ead2ce40e10bd93da2f7878a0231cce07cc026
Rohan Joby <rohanjobyak@gmail.com>
2021-05-24 21:02:06 +0530

    wip: store input list in Local Storage of browser
    Ensure data is available on site re-load

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e){
     e.preventDefault();
@@ -12,17 +12,19 @@ function addItem(e){
     }
     items.push(item);
     populateItems(items,itemsList);
-    this.rese();
+    localStorage.setItem('items',JSON.stringify(items));
+    this.reset();
 }
 
 function populateItems(items=[], itemList){
     itemList.innerHTML = items.map((item,index) => {
         return `
         <li>
-            <input type="checkbox" data-index=${index} id="item${index}" ${item.done ? "checked":"" }/>
-            <label for="item${index}">${item.text}</label>
+        <input type="checkbox" data-index=${index} id="item${index}" ${item.done ? "checked":"" }/>
+        <label for="item${index}">${item.text}</label>
         </li>`
     }).join('');
 }
 
 addItems.addEventListener('submit',addItem);
+populateItems(items,itemsList);
