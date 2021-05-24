a7705adfdefc978c18f695d7d935a53d8276cfdc
Rohan Joby <rohanjobyak@gmail.com>
2021-05-24 20:27:05 +0530

    wip: add function to read input and display

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
 
+function addItem(e){
+    e.preventDefault();
+    const text= (this.querySelector("[name=item")).value;
+    //console.log(text);
+    const item={
+        text:text,
+        done:false
+    }
+    items.push(item);
+    populateItems(items,itemsList);
+    console.log(item);
+}
+
+function populateItems(items=[], itemList){
+    itemList.innerHTML = items.map((item,index) => {
+        return `
+        <li>
+            <label for=">${item.text}</label>
+        </li>`
+    }).join('');
+}
+
+addItems.addEventListener('submit',addItem);
